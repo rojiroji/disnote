d9cd3631dc57816fdc1dd671a22b9d8ef7d132a9
rojiroji <mail@roji2.undo.jp>
2022-10-07 23:13:18 +0900

    起動時にも新しいバージョンの宣伝をするようにした

--- src/all.py
+++ src/all.py
@@ -176,9 +176,38 @@ def convert(recognizeThreads):
 			logger.error("{} の音声変換(4)に失敗しました({})。".format(input_file,e.with_traceback(tb)))
 			raise
 
+# 新しいバージョンがあったら表示する
+def announceNewVersion():
+	try:
+		r = requests.get('https://roji3.jpn.org/disnote/version.cgi', timeout=1) # 公開されている最新のzipファイルのファイル名が返る(例：DisNOTE_1.2.3.zip)
+		
+		if r.status_code == 200:
+			version = "v" + r.text.replace("DisNOTE_","").replace(".zip","")
+			zipVersion = version.replace("v","").split('.') # ファイル名からバージョンを取得
+			thisVersion = common.getVersion().replace("v","").split('.')
+			
+			for i in range(3): # メジャー、マイナー、パッチ の順で数字で比較
+				if int(zipVersion[i]) > int(thisVersion[i]): # 公開されているzipの方が新しい
+					print("----------------------------------------------------")
+					print("  新しいDisNOTE({}) が公開されているようです".format(version) )
+					print("  https://roji3.jpn.org/disnote/")
+					print("----------------------------------------------------")
+					break
+				if int(zipVersion[i]) < int(thisVersion[i]): # 公開されているzipの方が古い（普通は無いはず）
+					break
+		else:
+			#logger.info("DisNOTE最新バージョンの取得に失敗：{}".format(r.status_code))
+			pass
+
+	except:
+		#tb = sys.exc_info()[2]
+		#logger.error(traceback.format_exc())
+		pass # 何が起きても無視
+		
 
 # ここからメイン処理
 try:
+	announceNewVersion()
 	common.writeDefaultSysConfig() # とりあえず設定ファイルを読んで未設定の値を書き込こむ
 	
 	if len(sys.argv) < 2:
@@ -312,30 +341,4 @@ try:
 		sys.exit(1)
 
 finally: # バージョン確認する
-
-	try:
-		r = requests.get('https://roji3.jpn.org/disnote/version.cgi', timeout=1) # 公開されている最新のzipファイルのファイル名が返る
-		
-		if r.status_code == 200:
-			version = "v" + r.text.replace("DisNOTE_","").replace(".zip","")
-			zipVersion = version.replace("v","").split('.') # ファイル名からバージョンを取得
-			thisVersion = common.getVersion().replace("v","").split('.')
-			
-			for i in range(3): # メジャー、マイナー、パッチ の順で数字で比較
-				if int(zipVersion[i]) > int(thisVersion[i]): # 公開されているzipの方が新しい
-					print("----------------------------------------------------")
-					print("  新しいDisNOTE({}) が公開されているようです".format(version) )
-					print("  https://roji3.jpn.org/disnote/")
-					print("----------------------------------------------------")
-					break
-				if int(zipVersion[i]) < int(thisVersion[i]): # 公開されているzipの方が古い（普通は無いはず）
-					break
-		else:
-			#logger.info("DisNOTE最新バージョンの取得に失敗：{}".format(r.status_code))
-			pass
-
-	except:
-		#tb = sys.exc_info()[2]
-		#logger.error(traceback.format_exc())
-		pass # 何が起きても無視
-		
+	announceNewVersion()
