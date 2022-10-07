6f0473e7847d38698c8545a91d2582f1cea807e8
rojiroji <mail@roji2.undo.jp>
2022-10-07 23:19:55 +0900

    v2.3.0

--- DisNOTE.bat
+++ DisNOTE.bat
@@ -2,6 +2,7 @@ cd /d %~dp0
 
 py -m pip install --upgrade pip > stdout.txt
 py -m pip install inaSpeechSegmenter ffmpeg-python SpeechRecognition mutagen requests > stdout.txt
+py -m pip install git+https://github.com/openai/whisper.git > stdout.txt
 
 py src/all.py %* 2> stderr.txt
 PAUSE
